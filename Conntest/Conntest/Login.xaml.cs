d5a3253b47932a80a1bc8010c2cfb486bde13686
Abdullah Ajmeri <aajmeri@asu.edu>
2017-02-12 15:15:11 -0700

    Add project files.

---
+++ Conntest/Conntest/Login.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+using Xamarin.Forms;
+
+namespace Conntest
+{
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+        void Login_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Worked", "worked", "OK");
+            //new I18N.West.CP1250();
+
+            MySqlConnection conn;
+
+            conn = new MySql.Data.MySqlClient.MySqlConnection("Server=54.215.148.52; Port=3306; Database=sql3158647; Uid=sql3158647; Pwd=jVWM7BrzzU;");
+
+            try
+            {
+
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                    Output.Text = "SUCCESS CONNECT";
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Output.Text = ex.ToString();
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+	}
+}
